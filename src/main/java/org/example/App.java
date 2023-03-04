c1ac04eeb835688b69c53f3e9516c6f41a3a7c04
heeyeon3050 <kho200a@naver.com>
2023-03-05 00:20:04 +0900

    5단계

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,11 +3,15 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     private BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    List<WiseSaying> list = new ArrayList<>();
 
     public void run() throws IOException {
+
         System.out.println("== 명언 앱 ==");
         long id = 1;
 
@@ -21,9 +25,19 @@ public class App {
                 System.out.print("명언 : ");
                 String content = br.readLine().trim();
                 System.out.print("작가 : ");
-                String authorName = br.readLine().trim();
+                String author = br.readLine().trim();
+
+                WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                list.add(wiseSaying);
 
                 System.out.println(id++ + "번 명언이 등록되었습니다.");
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-".repeat(20));
+
+                for (int i = list.size() - 1; i >= 0; i--) {
+                    System.out.println(list.get(i));
+                }
             }
         }
     }
---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,31 @@
+package org.example;
+
+
+public class WiseSaying {
+    private long id;
+    private String content;
+    private String author;
+
+    public WiseSaying(long id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthorName() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return id + " / " + author + " / " + content;
+    }
+}
